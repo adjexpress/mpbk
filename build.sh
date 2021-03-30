ff97d8bc2f537c7e310530f982577353c75efd54
adjexpress <adjexpress@gmail.com>
2021-03-31 00:46:45 +0430

    LCD:g101evn01.3

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 source .env
+rm -rf arch/arm64/boot/dts/rockchip/rk3399-firefly-aiojd4-basir.dtb resource.img boot.img
 make firefly_linux_defconfig
 make rk3399-firefly-aiojd4-basir.img -j$CPU_CORES
+./flash.sh
